0d40f0c400b4dad0b0c5c5fc26bf1127e51d0154
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-10-13 18:39:48 +1000

    refactor(utils): rename deep sort

--- src/utils/deepSort.ts
+++ src/utils/deepSort.ts
@@ -4,12 +4,12 @@ function defaultSortFn(a: string, b: string) {
   return a.localeCompare(b);
 }
 
-function sort(src: any, comparator = defaultSortFn): any {
-  var out: { [x: string]: any };
+function deepSort(src: any, comparator = defaultSortFn): any {
+  let out: { [x: string]: any };
 
   if (Array.isArray(src)) {
     return src.map(function (item) {
-      return sort(item, comparator);
+      return deepSort(item, comparator);
     });
   }
 
@@ -19,7 +19,7 @@ function sort(src: any, comparator = defaultSortFn): any {
     Object.keys(src)
       .sort(comparator)
       .forEach(function (key) {
-        out[key] = sort(src[key], comparator);
+        out[key] = deepSort(src[key], comparator);
       });
 
     return out;
@@ -28,4 +28,4 @@ function sort(src: any, comparator = defaultSortFn): any {
   return src;
 }
 
-export default sort;
+export default deepSort;
