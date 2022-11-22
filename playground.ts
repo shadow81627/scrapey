9d78ce84bec1f4861b50ce790af270526d24368e
Damien Robinson <damien.robinson@daim.dev>
2022-11-22 13:38:03 +1000

    chore(playground): delete duplicate urls

--- playground.ts
+++ playground.ts
@@ -1,9 +1,24 @@
-import getFiles from './src/utils/getFiles';
+import { createConnection, In } from 'typeorm';
+import { Url } from './src/db/entity/Url';
+
+async function deleteDuplicateUrls() {
+  const disallowedHosts = [
+    'twitter.com',
+    'facebook.com',
+    'pinterest.com',
+    'woolworthsrewards.com.au',
+  ];
+  const connection = await createConnection();
+  const links = await connection.manager.find(Url, {
+    where: [
+      {
+        hostname: In(disallowedHosts),
+      },
+    ],
+  });
+  await connection.manager.softRemove(links);
+}
 
 (async () => {
-  const files = [];
-  for await (const filename of getFiles('content')) {
-    files.push(filename);
-  }
-  console.log('files', files.length);
+  await deleteDuplicateUrls();
 })();
