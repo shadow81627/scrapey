92073d963fd49a7f648f3c9a7179764266f3c7fd
Damien Robinson <damien.robinson@daim.dev>
2022-11-23 13:48:01 +1000

    fix(db data source): import entity

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,4 +1,33 @@
-import { DataSource } from "typeorm"
-import ormconfig from "../../ormconfig.json"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Url } from './entity/Url';
+import { Thing } from './entity/Thing';
+import { Recipe } from './entity/Recipe';
+import { Image } from './entity/Image';
+import { Product } from './entity/Product';
+import { CrawlIssue } from './entity/CrawlIssue';
+import { Organization } from './entity/Organization';
+import { Offer } from './entity/Offer';
 
-const MyDataSource = new DataSource(ormconfig)
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'example',
+  database: 'scrapey',
+  synchronize: true,
+  logging: false,
+  entities: [
+    Url,
+    Thing,
+    Recipe,
+    Image,
+    Product,
+    CrawlIssue,
+    Organization,
+    Offer,
+  ],
+});
+
+export default AppDataSource;
