28b1eb913dc05cadb55bc626539c4131ab845b5a
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 17:07:32 +1000

    fix(content service): check if file is same for updated at

--- src/content.service.ts
+++ src/content.service.ts
@@ -1,17 +1,23 @@
-import { promises as fsPromises, } from 'fs';
+import { promises as fsPromises, existsSync } from 'fs';
 const { readFile, mkdir, writeFile } = fsPromises;
 import Thing from './models/Thing';
 import deepSort from './utils/deepSort';
 
 export default class ContentService {
-  static async load({ slug, folder }: { slug: string; folder: string }): Promise<Thing | undefined> {
+  static async load({
+    slug,
+    folder,
+  }: {
+    slug: string;
+    folder: string;
+  }): Promise<Thing | undefined> {
     try {
-      return JSON.parse(await readFile(`${folder}/${slug}.json`, {
-        encoding: 'utf8',
-      }));
-    } catch (_) {
-
-    }
+      return JSON.parse(
+        await readFile(`${folder}/${slug}.json`, {
+          encoding: 'utf8',
+        }),
+      );
+    } catch (_) {}
   }
 
   static async save({
@@ -24,8 +30,22 @@ export default class ContentService {
     folder: string;
   }): Promise<void> {
     await mkdir(folder, { recursive: true });
+    const dataPath = `${folder}/${slug}.json`;
+    const oldData = existsSync(dataPath)
+      ? JSON.parse(
+          await readFile(dataPath, {
+            encoding: 'utf8',
+          }),
+        )
+      : {};
+    const same =
+      JSON.stringify(deepSort({ ...oldData, updatedAt: undefined })) ===
+      JSON.stringify(deepSort({ ...data, updatedAt: undefined }));
+    if (same) {
+      data.updatedAt = oldData.updatedAt;
+    }
     return await writeFile(
-      `${folder}/${slug}.json`,
+      dataPath,
       JSON.stringify(deepSort(data), undefined, 2) + '\n',
     );
   }
