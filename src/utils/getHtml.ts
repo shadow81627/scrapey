9fae6a12fd58631ba2a4945ca32cf29609c4d7c8
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 15:30:42 +1000

    fix(utils getHtml): save and reuse cookies

--- src/utils/getHtml.ts
+++ src/utils/getHtml.ts
@@ -1,4 +1,7 @@
 import { firefox } from 'playwright';
+import { promises as fsPromises, } from 'fs';
+import deepSort from './deepSort';
+const { readFile, writeFile } = fsPromises;
 
 interface getHtmlArgs {
   url: string;
@@ -10,15 +13,27 @@ export default async function getHtml({ url }: getHtmlArgs): Promise<string> {
     slowMo: 250,
     waitUntil: 'networkidle0',
     defaultViewport: null,
+    maxRedirects: url.startsWith('https://shop.coles.com.au') ? 0 : undefined,
   };
   const timeout = 120000; // timeout in milliseconds.
   const waitForTimeout = 15000;
   const browser = await firefox.launch(browserOptions);
+  const context = await browser.newContext();
   try {
+    
     // fetch browser rendered html
-    const page = await browser.newPage();
+    const page = await context.newPage();
+
+    const deserializedCookies = JSON.parse((await readFile('cookies.json')).toString());
+    await context.addCookies(deserializedCookies)
+
     await page.goto(url, { waitUntil: 'domcontentloaded', timeout });
     await page.waitForTimeout(waitForTimeout);
+
+    const cookies = await context.cookies()
+    const cookieJson = JSON.stringify(deepSort(cookies), undefined, 2) + '\n'
+    writeFile('cookies.json', cookieJson)
+
     return await page.content();
   } finally {
     await browser.close();
