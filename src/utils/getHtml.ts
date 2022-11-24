b83f028a93bd49009b069d78031bf61e709104ee
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 16:52:00 +1000

    fix(util get html): sort cookies

--- src/utils/getHtml.ts
+++ src/utils/getHtml.ts
@@ -1,6 +1,7 @@
 import { firefox } from 'playwright';
-import { promises as fsPromises, } from 'fs';
+import { promises as fsPromises } from 'fs';
 import deepSort from './deepSort';
+import { sortBy } from 'lodash';
 const { readFile, writeFile } = fsPromises;
 
 interface getHtmlArgs {
@@ -20,19 +21,21 @@ export default async function getHtml({ url }: getHtmlArgs): Promise<string> {
   const browser = await firefox.launch(browserOptions);
   const context = await browser.newContext();
   try {
-    
     // fetch browser rendered html
     const page = await context.newPage();
 
-    const deserializedCookies = JSON.parse((await readFile('cookies.json')).toString());
-    await context.addCookies(deserializedCookies)
+    const deserializedCookies = JSON.parse(
+      (await readFile('cookies.json')).toString(),
+    );
+    await context.addCookies(deserializedCookies);
 
     await page.goto(url, { waitUntil: 'domcontentloaded', timeout });
     await page.waitForTimeout(waitForTimeout);
 
-    const cookies = await context.cookies()
-    const cookieJson = JSON.stringify(deepSort(cookies), undefined, 2) + '\n'
-    writeFile('cookies.json', cookieJson)
+    const cookies = await context.cookies();
+    const cookieJson =
+      JSON.stringify(sortBy(deepSort(cookies), 'name'), undefined, 2) + '\n';
+    writeFile('cookies.json', cookieJson);
 
     return await page.content();
   } finally {
