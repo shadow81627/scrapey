42d67d8397c9d34249e0ebb25ae268492bf18360
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 15:28:22 +1000

    feat(playground): set coles national urls

---
+++ src/playground/colesNationalReplaceUrls.ts
@@ -0,0 +1,32 @@
+import { Like, Not } from 'typeorm';
+import AppDataSource from '../db/data-source';
+import { Url } from '../db/entity';
+
+export default async function colesNationalReplaceUrls(): Promise<void> {
+  const connection = AppDataSource;
+  const links = await connection
+    .getRepository(Url)
+    .createQueryBuilder('url')
+    .where({ pathname: Like('/a/%/%') })
+    .andWhere({
+      pathname: Not(Like('/a/national/%')),
+    }).getMany();
+
+  console.log('coles national replace total', links.length);
+  const regex = /^\/a\/(.+?)\//;
+  let iteration = 0;
+  for (const link of links) {
+    iteration++;
+    const replaced = link.pathname?.replace(regex, '/a/national/');
+    if (replaced !== link.pathname) {
+      link.pathname = replaced;
+      link.crawledAt = null;
+      link.duration = null;
+      // await connection.manager.save(link);
+      console.log(iteration, link.url);
+    }
+
+    
+  }
+  // await connection.manager.softRemove(links);
+}
