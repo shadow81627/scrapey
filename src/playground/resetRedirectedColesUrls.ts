fe5cb1302e773daeee499dab62991c3ca56a0d60
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 16:04:27 +1000

    feat(playground): add reset redirect coles urls

---
+++ src/playground/resetRedirectedColesUrls.ts
@@ -0,0 +1,20 @@
+import AppDataSource from "../db/data-source";
+import { Url } from "../db/entity";
+
+export default async function resetRedirectedColesUrls(): Promise<void> {
+  console.log('fetch redirected coles urls to reset');
+  const connection = AppDataSource;
+  const links = await connection.manager.find(Url, {
+    relations: { canonical: true },
+    where: {
+      canonical: { id: '5c3954dd-4c02-5b3e-a8be-42bde5a02dc6'}
+    }
+  });
+  console.log('total redirected coles urls', links.length)
+  for (const link of links) {
+    link.canonical = null;
+    link.crawledAt = null;
+    link.duration = null;
+    await connection.manager.save(link);
+  }
+}
