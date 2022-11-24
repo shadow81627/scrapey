b91798b0a89c8ef50440db96b258158dd6add794
Damien Robinson <damien.robinson@daim.dev>
2022-11-24 15:30:58 +1000

    chore(playground): call functions

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,8 +1,16 @@
 import AppDataSource from '../db/data-source';
 import deleteDisallowedDomains from './deleteDisallowedDomains';
+import deleteDuplicateUrls from './deleteDuplicateUrls';
+import colesNationalReplaceUrls from './colesNationalReplaceUrls';
+import incorrectUIrlIds from './incorrectUrlId';
 
 (async () => {
   await AppDataSource.initialize();
+
   await deleteDisallowedDomains();
+  await colesNationalReplaceUrls();
+  await deleteDuplicateUrls();
+  await incorrectUIrlIds();
+
   await AppDataSource.destroy();
 })();
