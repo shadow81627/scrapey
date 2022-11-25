88db03c958c21ec450d4a3c95ff4620017b9f30a
Damien Robinson <damien.robinson@daim.dev>
2022-11-26 00:28:00 +1000

    fix(playground missing urls): only fetch urls if not image or thing

--- src/playground/urlsMissingUrls.ts
+++ src/playground/urlsMissingUrls.ts
@@ -5,7 +5,7 @@ import { Url } from '../db/entity';
 /**
  * Check if a url has stored any urls. If missing urls likely means the page had
  * issues being crawled e.g got blocked or hit a transient error.
- * @returns 
+ * @returns
  */
 export default async function urlsMissingUrls(): Promise<string> {
   const connection = AppDataSource;
@@ -25,15 +25,17 @@ export default async function urlsMissingUrls(): Promise<string> {
   let iteration = 0;
   for (const link of links) {
     iteration++;
-    const urls = await link.urls;
     const image = await link.image;
     const things = await link.things;
-    if ((!urls || !urls.length) && link.crawledAt && !image && !things) {
-      empties.push(link);
-      console.log(iteration, link.url);
-      link.crawledAt = null;
-      link.duration = null;
-      // await connection.manager.save(link);
+    if (!image && !things && link.crawledAt) {
+      const urls = await link.urls;
+      if (!urls || !urls.length) {
+        empties.push(link);
+        console.log(iteration, link.url);
+        link.crawledAt = null;
+        link.duration = null;
+        // await connection.manager.save(link);
+      }
     }
   }
   const response = `urls missing urls ${empties.length}`;
