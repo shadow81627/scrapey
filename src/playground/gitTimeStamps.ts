f59d03ed7713b7512e2718c0eed88ec75b5d0c2f
Damien Robinson <damien.robinson@daim.dev>
2022-11-26 00:33:35 +1000

    fix(playground git time stamps): only write to file if data different

--- src/playground/gitTimeStamps.ts
+++ src/playground/gitTimeStamps.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs/promises'; 
+import { readFile, writeFile } from 'fs/promises';
 import deepSort from '../utils/deepSort';
 import getFiles from '../utils/getFiles';
 import util from 'util';
@@ -14,7 +14,7 @@ const execAsync = util.promisify(exec);
  * @returns file create date
  */
 async function gitCreateDate(relativePath: string): Promise<Date> {
-  const command = `git log --follow --format=%ad --date default ${relativePath} | tail -1`
+  const command = `git log --follow --format=%ad --date default ${relativePath} | tail -1`;
   const { stdout, stderr } = await execAsync(command);
   if (stderr) {
     throw new Error(stderr);
@@ -24,22 +24,22 @@ async function gitCreateDate(relativePath: string): Promise<Date> {
 
 export default async function gitTimeStamps(): Promise<void> {
   let iteration = 0;
+  console.log('fetching createdAt from git history');
   for await (const filename of getFiles('content')) {
-    iteration++
+    iteration++;
     try {
-      const content = JSON.parse(
-        (await readFile(filename)).toString(),
-      );
-      const relativePath = path.relative(process.cwd(), filename)
-      content.createdAt = await gitCreateDate(relativePath);
-      console.log(iteration, filename)
-      await writeFile(
-        filename,
-        JSON.stringify(deepSort(content), undefined, 2) + '\n',
-      )
+      const content = JSON.parse((await readFile(filename)).toString());
+      const relativePath = path.relative(process.cwd(), filename);
+      const createdAt = (await gitCreateDate(relativePath)).toISOString();
+      if (content.createdAt !== createdAt) {
+        console.log(iteration, relativePath);
+        await writeFile(
+          filename,
+          JSON.stringify(deepSort(content), undefined, 2) + '\n',
+        );
+      }
     } catch (error) {
       console.error(error);
     }
-    
   }
 }
