46ae8c89da2da72ff42ab7f64b9efdbc08c206ed
Damien Robinson <damien.robinson@daim.dev>
2022-11-29 17:18:56 +1000

    fix(playground git timestamps): check git log output length

--- src/playground/gitTimeStamps.ts
+++ src/playground/gitTimeStamps.ts
@@ -13,13 +13,15 @@ const execAsync = util.promisify(exec);
  * @param filename to get create date from git
  * @returns file create date
  */
-async function gitCreateDate(relativePath: string): Promise<Date> {
+async function gitCreateDate(relativePath: string): Promise<string | undefined> {
   const command = `git log --follow --format=%ad --date default ${relativePath} | tail -1`;
   const { stdout, stderr } = await execAsync(command);
   if (stderr) {
     throw new Error(stderr);
   }
-  return new Date(stdout);
+  if (stdout && stdout.length) {
+    return new Date(stdout).toISOString();
+  }
 }
 
 export default async function gitTimeStamps(): Promise<void> {
@@ -30,8 +32,8 @@ export default async function gitTimeStamps(): Promise<void> {
     const relativePath = path.relative(process.cwd(), filename);
     try {
       const content = JSON.parse((await readFile(filename)).toString());
-      const createdAt = (await gitCreateDate(relativePath)).toISOString();
-      if (content.createdAt !== createdAt) {
+      const createdAt = await gitCreateDate(relativePath);
+      if (createdAt && content.createdAt !== createdAt) {
         console.log(iteration, relativePath);
         content.createdAt = createdAt
         await writeFile(
