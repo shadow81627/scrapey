e16b7cd5b8a45da66a324d2e3dd85e0b64db99f4
Damien Robinson <damien.robinson@daim.dev>
2022-11-30 13:23:46 +1000

    fix(playground sql count url urls): add deleted at filter

--- src/playground/sql/count-url-urls.sql
+++ src/playground/sql/count-url-urls.sql
@@ -6,6 +6,8 @@ SELECT a.hostname,
 FROM url_urls l
 JOIN url a ON l.urlId_1 = a.id
 JOIN url b ON l.urlId_2 = b.id
+WHERE a.deletedAt IS NULL
+  AND b.deletedAt IS NULL
 GROUP BY a.hostname,
          a.pathname,
          a.search
